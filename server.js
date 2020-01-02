1c9a3ce6f7f00ffc72cd8300fd8c998ff05acd70
asharaloran96 <asharoran96@gmail.com>
2020-01-02 20:12:41 +0200

    try in the server data

--- server.js
+++ server.js
@@ -11,15 +11,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static('public'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
+app.get('/searches/new', searchForm); // function one 
+app.post('/searches', getDataFromForm); // function two 
+app.get ('/' , getAllBooks); // function three 
+app.get('/books/detail' , addBook); // function four
+app.post('/books/detail' , processBook); // function five
+app.get('/books/show/:books_id' , addBookById) // function six
 
-    res.render('pages/index')
-
-
-});
-
-app.post('/searches',(req, res) => {
-
+function handleError(error, response){
+    response.render('pages/error', {error: error});
+}
+// function one new.ejs
+function searchForm (req, res) {
+    res.render('pages/searches/new');
+};
+// function two
+function getDataFromForm(req, res) {
     const url = `https://www.googleapis.com/books/v1/volumes?q=${req.body.selectBy}+${req.body.input}`;
     superagent.get(url)
         .then(data => {
@@ -27,8 +34,43 @@ app.post('/searches',(req, res) => {
             let book = element.map(data => new Book(data));
             res.render('pages/searches/show', { books: book });
         })
-    });
+    };
+// function three 
+function getAllBooks(req ,res){
+    let SQL = `SELECT * FROM books ;`;
+    client.query(SQL)
+    .then( data => {
+        res.render('index' , {books : data.rows});
+    }).catch(err => handleError(err));
+}
+// function four
+function addBook(req , res){
+    res.render('pages/books/detail');
+}
+// function five
+function processBook (req ,res){
+    let {title, author, isbn, image_url, description, bookshelf} =req.body;
+    let SQL = `INSERT INTO books (title, author, isbn, image_url, description, bookshelf) VALUES ($1 , $2 , $3 , $4 , $5 ,$6) ;`;
+    let values = [title, author, isbn, image_url, description, bookshelf];
+    client.query(SQL ,values)
+    .then( () => {
+        res.redirect('/');
+    }).catch( err => handleError(err));
+}
 
+// function six
+function addBookById( req ,res){
+    // from database books_id_seq
+    let id = req.params.books_id;
+    let SQL =`SELECT * FROM books WHERE id=$1`;
+    let values = [id];
+    client.query(SQL ,values)
+    .then ( data =>{
+        res.render( 'pages/books/show' , { books : data.rows[0]})
+    }).catch(err => handleError(err));
+
+}
+// constractuer function 
         function Book(data) {
             // The if statment inside this function from the demo // but it's really amazing and we learn sth new 
             this.title = data.volumeInfo.title ? data.volumeInfo.title : "No Title Available";
@@ -36,11 +78,6 @@ app.post('/searches',(req, res) => {
             this.authors = data.volumeInfo.authors ? data.volumeInfo.authors : "No Authors";
             this.desc = data.volumeInfo.description ? data.volumeInfo.description : "No description available";
         }
-
-
-
-
-
 app.get('*', (req, res) => {
     res.status(404).send('not found');
 });
